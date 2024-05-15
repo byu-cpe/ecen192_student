af08ef63da38fd388529db9050a17de1a276e3e6
Jeff Goeders <jeff.goeders@gmail.com>
2024-05-15 16:27:42 -0600

    Initial code commit for esp32

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "ybadge.h"
+
+int main() {
+  // This function should call ONE of the functions below.
+  // Uncomment or comment the lines so that just the function
+  // you want to call is uncommented.
+
+  test_all_features();
+  // badge_activity();
+
+  while (1)
+    ;
+}
